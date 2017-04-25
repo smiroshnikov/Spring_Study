01005a27430eb2a8583e02fb7f48aa0eb37f9362
smiroshn <smiroshn@akamai.com>
2017-04-25 14:25:42 +0300

    instructions to verify . TODO add unit test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,5 +8,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        // just navigate to "http://localhost:8080/greeting"
     }
 }
