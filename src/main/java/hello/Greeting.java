2968277c856f7b296e39feaf0298266daef95e48
smiroshn <smiroshn@akamai.com>
2017-04-25 14:24:12 +0300

    Initial commit

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,25 @@
+package hello;
+
+/**
+ * Created by smiroshn on 4/25/17.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+
+}
+
